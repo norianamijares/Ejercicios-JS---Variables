d369c0e116df4b2c97f1b398c617f11d1a3abb72
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-02 17:28:24 -0300

    Cálculo del área de un rectángulo

---
+++ js/ejercicio1.js
@@ -0,0 +1,9 @@
+let base = prompt("Ingrese la base del rectángulo: ");
+let altura = prompt("Ingrese la altura del rectángulo: ");
+
+base = parseFloat(base);
+altura = parseFloat(altura);
+
+let area = base * altura;
+
+console.log("El área del rectángulo es: " + area);
