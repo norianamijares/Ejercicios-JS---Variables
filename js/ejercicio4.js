0572cda3f7abe8660dd6c6120b054e8c21f295f7
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-02 18:18:42 -0300

    Creé las variables para mi color, comida y lugar favorito, creé la frase usando estas variables, mostré la frase en la consola

---
+++ js/ejercicio4.js
@@ -0,0 +1,7 @@
+const miColorFavorito = "rosa";
+const miComidaFavorita = "pollo frito";
+const miLugarFavorito = " la playa";
+
+const frase = `Mi color favorito es ${miColorFavorito}, mi comida favorita es ${miComidaFavorita} y mi lugar favorito es${miLugarFavorito}.`;
+
+console.log(frase);
