ed814b112981828f543758a1578ef49592161007
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-02 18:25:25 -0300

    Creé las variables numero 1, 2, y 3, convertí los valores ingresados por el usuario a valores numéricos, calculé el promedio y mostré el resultado en la consola

---
+++ js/ejercicio5.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Introduce un número: ");
+let num2 = prompt("Introduce otro número: ");
+let num3 = prompt("Introduce un tercer número: ");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+num3 = parseInt(num3);
+
+let promedio = (num1 + num2 + num3) / 3;
+
+console.log("El promedio de los tres números es: " + promedio);
