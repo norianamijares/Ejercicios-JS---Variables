04721de899d2f6dd2e510bc3c77e40285f7a94d4
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-06 18:40:01 -0300

    Creé la variable radio, convertí el valor ingresado por el usuario a un valor numérico, creé la variable perimetro y mostré el resultado en la consola

---
+++ js/ejercicio6.js
@@ -0,0 +1,7 @@
+let radio = prompt("Introduce el radio del círculo: ");
+
+radio = parseFloat(radio);
+
+let perimetro = 2 * Math.PI * radio;
+
+console.log("El perímetro del círculo es: " + perimetro);
