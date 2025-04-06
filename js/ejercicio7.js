6b5d8acad05ba6a409e8a44bf02a33406ffb25af
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-06 19:07:04 -0300

    ejercicio 7 terminado

---
+++ js/ejercicio7.js
@@ -0,0 +1,6 @@
+let minutos = prompt ("Ingresa una cantidad de minutos");
+
+let segundos = minutos * 60;
+let horas = minutos / 60;
+
+alert(`La cantidad de minutos ingresada equivale a ${segundos} segundos y ${horas} horas`);
