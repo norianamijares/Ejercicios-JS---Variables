46492ab9207541753991df633faa69b07be8b12d
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-07 19:13:37 -0300

    ejercicio 10 finalizado

---
+++ js/ejercicio10.js
@@ -0,0 +1,14 @@
+let auto = {
+    marca: "Ford",
+    modelo: "Mustang GT",
+    año: 2022,
+    color: "Negro",
+    motor: "V8 5.0L",
+    caballosDeFuerza: 480,
+    tipo: "Deportivo",
+    peso: "1650 kg",
+};
+
+console.log("Información del auto:", auto);
+
+document.write(`El modelo ${auto.modelo} de ${auto.marca} tiene un poderoso motor ${auto.motor}, ideal para los amantes de los autos ${auto.tipo}.`)
