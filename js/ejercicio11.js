9c52bd2071817cea0562e0963f2257bcddd1355a
Noriana Mijares <norianamijares1998@gmail.com>
2025-04-07 19:26:31 -0300

    ejercicio 11 finalizado

---
+++ js/ejercicio11.js
@@ -0,0 +1,24 @@
+const listaDeCompras = [
+    "Pasta",
+    "Huevos",
+    "Papel higienico",
+    "Atún",
+    "Salsa para pasta",
+    "Café",
+    "Pollo",
+    "Rapiditas",
+    "Queso",
+    "Salame", 
+];
+
+console.log("Lista de compras:");
+console.log(listaDeCompras[0]);
+console.log(listaDeCompras[1]);
+console.log(listaDeCompras[2]);
+console.log(listaDeCompras[3]);
+console.log(listaDeCompras[4]);
+console.log(listaDeCompras[5]);
+console.log(listaDeCompras[6]);
+console.log(listaDeCompras[7]);
+console.log(listaDeCompras[8]);
+console.log(listaDeCompras[9]);
